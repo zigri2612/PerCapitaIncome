60b8da07b3786a1fa71f4c69b47801d258d88161
Prince <prince_gupta2612@yahoo.in>
2019-11-19 16:24:13 +0530

    Delete download-gdrive.sh

--- download-gdrive.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-fileidl a="11YG5okUSFiH8kUSYAWQMza226edh78pJ"
-filename="AppleSchoolWeb"
-curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev
-curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
